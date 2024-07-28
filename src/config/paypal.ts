9cfa79a979b1518146f79c711925f7ce542325d1
Alex Medina <alxmed8@gmail.com>
2024-07-28 10:11:42 -0500

    Cambio modo de pago a live

--- src/config/paypal.ts
+++ src/config/paypal.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 paypal.configure({
-  mode: 'sandbox', // Cambiar a 'live' para producción
+  mode: 'live', // Cambiar a 'live' para producción
   client_id: process.env.PAYPAL_CLIENT_ID || '',
   client_secret: process.env.PAYPAL_CLIENT_SECRET || ''
 });
